fc9bcd1ca3c5b9f92d5a6ae60843bc086c411bee
Dart <skulldude327@gmail.com>
2026-04-17 12:04:46 -0500

    Fix

--- addons/main/script_component.hpp
+++ addons/main/script_component.hpp
@@ -3,7 +3,7 @@
 #include "script_mod.hpp"
 
 // #define DEBUG_MODE_FULL
-#define DISABLE_COMPILE_CACHE
+// #define DISABLE_COMPILE_CACHE
 // #define ENABLE_PERFORMANCE_COUNTERS
 
 #include "\a3\ui_f\hpp\defineCommonGrids.inc"
